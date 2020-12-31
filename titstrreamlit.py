45e82069f31f929a79a00912279be9dd330463d1
BelnaJohn <76556054+BelnaJohn@users.noreply.github.com>
2020-12-31 13:30:00 +0530

    Update titstrreamlit.py

--- titstrreamlit.py
+++ titstrreamlit.py
@@ -8,7 +8,7 @@ Created on Tue Dec 29 23:28:15 2020
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
+#import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
